17256928f9c72d32ae1764a9135d66c1f5777b96
Caio Franca <caio.batista@radixeng.com.br>
2021-03-03 00:15:42 -0300

    Versão Inicial MoveIt - NLW#04

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext)
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/c4iofranca.png" alt="Caio França"/>
+            <div>
+                <strong>Caio França</strong>
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    )
+}
